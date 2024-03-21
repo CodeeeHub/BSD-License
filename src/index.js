2e51017416d599adca77bd8fa96bd8357164098d
Farzin Hamzehi <124732366+FARZINzx@users.noreply.github.com>
2024-03-21 12:50:38 +0330

    Feat : BSD License

---
+++ src/index.js
@@ -0,0 +1,33 @@
+// const images = document.getElementsByTagName("img");
+
+// const createSkeletonImage = (width, height) => {
+//   const img = document.createElement("img");
+//   img.classList.add(
+//     "bg-slate-200",
+//     "skeleton-image",
+//     "animate-pulse",
+//     "rounded-md"
+//   );
+//   img.setAttribute("width", width);
+//   img.setAttribute("height", height);
+//   return img;
+// };
+
+// for (let i = 0; i < images.length; i++) {
+//   const originalImage = images[i];
+//   const skeletonImage = createSkeletonImage(
+//     originalImage.width,
+//     originalImage.height
+//   );
+//   originalImage.parentNode.replaceChild(skeletonImage, originalImage);
+// }
+
+// const replaceWithActualImages = () => {
+//      for (let i = 0; i < images.length; i++) {
+//           const originalImg = images[i];
+//           console.log(originalImg)
+//      }
+//    };
+
+// setTimeout(replaceWithActualImages, 100);
+
